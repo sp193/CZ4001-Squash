74f82cf5e46f564513440755b883d2571f834c39
sp193 <pirorin187@gmail.com>
2018-02-21 22:41:42 +0800

    Added player controls (A+D) and racket animation (LMB).

---
+++ SquashGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //FixedUpdate() runs off its own timer, independent of the refresh rate. It is called more often than Update()
+    void FixedUpdate()
+    {
+        float moveHorizontal;
+
+        moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+        rb.AddForce(movement * speed);
+    }
+}
---
+++ SquashGame/Assets/Scripts/RacketController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacketController : MonoBehaviour {
+
+    private bool swinging, swingingBack;
+    private int rotationTickCount;
+    private Rigidbody rb;
+    public int rotationSpeed;
+    private int rotationTicks;
+
+    // Use this for initialization
+    void Start () {
+        swinging = false;
+        swingingBack = false;
+        rb = GetComponent<Rigidbody>();
+        rotationTicks = 90 / rotationSpeed;
+        Debug.Log("rotationTicks: " + rotationTicks);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButtonDown("Fire1"))
+        {   //Change control set.
+
+            if (!swinging && !swingingBack)
+            {
+                swinging = true;
+                swingingBack = false;
+                rotationTickCount = 0;
+            }
+        }
+
+        if(swinging)
+        {
+            if (rotationTickCount >= rotationTicks)
+            {
+                swinging = false;
+                swingingBack = true;
+            }
+            else
+            {
+                transform.Rotate(new Vector3(rotationSpeed, 0, 0));
+                rotationTickCount++;
+            }
+        }
+        if (swingingBack)
+        {
+            if (rotationTickCount <= 0)
+                swingingBack = false;
+            else
+            {
+                transform.Rotate(new Vector3(-rotationSpeed, 0, 0));
+                rotationTickCount--;
+            }
+        }
+    }
+}
