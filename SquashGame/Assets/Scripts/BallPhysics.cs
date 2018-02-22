ee6e3c96d89660478cd369e28220d8cf00f8b611
seahzd <seahzd@hotmail.com>
2018-02-22 14:32:21 +0800

    Set bounce on the ball

---
+++ SquashGame/Assets/Scripts/BallPhysics.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallPhysics : MonoBehaviour {
+    private Vector3 oldvel;
+    private Rigidbody myRigidbody;
+    private Vector3 prevSpd;
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        oldvel = myRigidbody.velocity;
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint cp = collision.contacts[0];
+
+        myRigidbody.velocity = Vector3.Reflect(oldvel, cp.normal);
+        prevSpd = myRigidbody.velocity;
+        myRigidbody.velocity += cp.normal * 10.0f;
+
+    }
+}
---
+++ SquashGame/Assets/Scripts/DetectHitBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectHitBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("ball")) {
+            //other.gameObject.GetComponent<Rigidbody>().AddForce(this.GetComponent<Rigidbody>().transform.position);
+            other.gameObject.GetComponent<Rigidbody>().velocity = (other.gameObject.GetComponent<Rigidbody>().velocity+this.GetComponent<Rigidbody>().velocity)*10;
+        }
+    }
+}
